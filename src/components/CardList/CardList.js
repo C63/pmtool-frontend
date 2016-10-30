c9aff7b1ff7e8205f53653337d88d9cb0b449cac
Chi Nguyen <Chi Nguyen>
2016-10-30 13:27:18 +0200

    add new task ondoing

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,19 +1,43 @@
 import React from 'react'
 import Card from './Card/Card'
+import NewCardModal from '../Modal/NewCardModal/NewCardModal'
 export default class CardList extends React.Component
 {
+  constructor () {
+    super()
+    this.state = {
+      isOpen: false
+    }
+    this.toggleModal = this.toggleModal.bind(this)
+  }
+  toggleModal () {
+    const { isOpen } = this.state
+    this.setState({
+      isOpen: !isOpen
+    })
+  }
+  renderCardModal () {
+    const { isOpen } = this.state
+    const { list } = this.props
 
+    if (isOpen) {
+      return <NewCardModal isOpen={isOpen} closeModal={this.toggleModal} cards={list.cards} />
+    }
+    return
+  }
   render () {
     const { list } = this.props
+    const NewCardModal = this.renderCardModal()
     return (
       <div className='card-list'>
         <div className='card-list__header'>
           <span className='card-list__header__name'>{list.list_name}</span>
-          <span className='card-list__header__counter'>{list.list_count}</span>
+          <span className='card-list__header__counter'>{list.cards.length}</span>
           <span className='card-list__header__right'>
-            <span className='plus'>+</span>
+            <span className='plus' onClick={this.toggleModal}>+</span>
             <span className='dots'>...</span>
           </span>
+          {NewCardModal}
         </div>
         { list.cards.map((card) => {
           return (
---
+++ src/components/Modal/NewCardModal/NewCardModal.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Modal } from 'react-bootstrap'
+import CoreModal from '../CoreModal'
+import UserList from '../../User/UserList/UserList'
+export default class NewCardModal extends React.Component
+{
+  renderCardModal () {
+    const { cards } = this.props
+    return (
+      <div className='new-card-modal'>
+        <Modal.Header closeButton>
+          <h2>Add new task</h2>
+        </Modal.Header>
+        <Modal.Body>
+          <div className='new-card-modal'>
+            <h3>Task name</h3>
+            <input type='text' placeholder='Lorem isum wao' />
+            <h3>Description</h3>
+            <input type='text' placeholder='Lirem isum dkm' />
+            <h3>People</h3>
+            <div className='new-card-modal__people'>
+              <UserList users={cards[0].users} />
+            </div>
+          </div>
+        </Modal.Body>
+      </div>
+    )
+  }
+  render () {
+    const { isOpen, closeModal } = this.props
+    const cardModalContent = this.renderCardModal()
+    return (
+      <CoreModal isOpen={isOpen} closeModal={closeModal} children={cardModalContent} />
+    )
+  }
+}
+
+NewCardModal.propTypes = {
+  isOpen : React.PropTypes.bool,
+  closeModal : React.PropTypes.func,
+  cards: React.PropTypes.array
+}
